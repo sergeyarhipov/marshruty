3742270dce44d0a13717b96b4c7e1cddd9e5709d
sergeyarhipov <s.m.arhipov@mail.ru>
2021-03-05 11:44:45 +0500

    Изменения по svg кнопки меню

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var navigationButton = document.querySelector(".navigation__button");
 var menu = document.querySelector(".menu");
 var mainHeader = document.querySelector(".main-header");
+var menuButton = document.querySelector(".navigation__button")
 
 navigationButton.addEventListener("click", function(evt) {
     if (menu.classList.contains("menu--invisible")) {
@@ -8,4 +9,8 @@ navigationButton.addEventListener("click", function(evt) {
     } else {
         menu.classList.add("menu--invisible");
     }
+});
+
+menuButton.addEventListener("click", function(evt) {
+    menuButton.classList.toggle("navigation__button--close");
 });
